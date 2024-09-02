a0d6988be92163f1fa0a1e15518d56339e12ba80
Jathosn <thoresen.jan.94@gmail.com>
2024-09-02 13:56:08 +0200

    Randomization implemented for question selection

--- Flashcard/Program.cs
+++ Flashcard/Program.cs
@@ -6,44 +6,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 namespace Flashcard
 {
     internal class Program
     {
+        public List<string> Questions { get; private set; } = new List<string>();
+        public List<string> Answers { get; private set; } = new List<string>();
+        public List<string> Hints { get; private set; } = new List<string>();
         static void Main(string[] args)
         {
+            Program program = new Program();
+
             string textFile = "C:/Users/JanThoresen/source/repos/Flashcard/Flashcard/Questions.txt";
             string[] lines = File.ReadAllLines(textFile);
             int totals = lines.Length / 3;
             Console.WriteLine($"So... you wish to memorize for a test? Please specify the number of questions to choose from. There are {totals} questions in total \n");
 
-            Information(textFile, lines);
+            program.Information(textFile, lines);
+            program.Randomizer();
 
             Console.ReadLine();
         }
-        static void Information(string textFile, string[] lines)
+         void Information(string textFile, string[] lines)
         {
-            List<string> questions = new List<string>();
-            List<string> answers = new List<string>();
-            List<string> hints = new List<string>();
 
             for (int i = 0; i < lines.Length; i++)
             {
                 if ((i + 1) % 3 == 0)
                 {
-                    answers.Add(lines[i]);
+                    Answers.Add(lines[i]);
                 }
                 else if ((i + 1) % 3 == 1)
                 {
-                    questions.Add(lines[i]);
+                    Questions.Add(lines[i]);
                 }
                 else
                 {
-                    hints.Add(lines[i]);
+                    Hints.Add(lines[i]);
                 }
             }
-            Console.WriteLine(string.Join(Environment.NewLine, questions));
+           // Console.WriteLine(string.Join(Environment.NewLine, Questions));
+        }
+        public void Randomizer()
+        {
+            Random rng = new Random();
+            int n = Questions.Count;
+
+            if (n > 0)
+            {
+                int randomIndex = rng.Next(n);
+                Console.WriteLine($"Random Question: {Questions[randomIndex]}");
+            }
         }
     }
 }
